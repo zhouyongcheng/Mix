d24fedced386e64ecf4c3ad45cc32b22f336aeeb
zhouyongcheng <yongchengzhou@hotmail.com>
2017-02-21 22:49:47 +0800

    add angular and mysql admin guide

---
+++ mysql/create/create_table.sql
@@ -0,0 +1,10 @@
+create table my_table (name varchar(10)) engine=memory;
+create table my_table (name varchar(10)) engine=innodb;
+create table my_table (name varchar(10)) engine=myisam;
+create table t_csv (id int not null default 0, v1 varchar(10) not null default '') engine=csv;
+create table t_name engine=myisam as select * from other_table;
+create table t_name engine=archive as select * from other_table;
+create table t_name (age int, c char(10)) engine=blackhole;
+
+
+
