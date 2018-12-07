35553aa4bd8f6b881209f7010a5108976cc151a5
zhouyongcheng <yongchengzhou@hotmail.com>
2018-12-08 07:55:02 +0800

    add create_database script

--- mysql/create/create_database.sql
+++ mysql/create/create_database.sql
@@ -1,5 +1,7 @@
 create database cmwin character set utf8;
 
+CREATE DATABASE cmwin DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
 
 create table todo (
 	todo varchar(40),
@@ -7,3 +9,9 @@ create table todo (
 	create_date date,
 	create_user varchar(30)
 ) engine=innodb;	
+
+
+# EXECUTE SQL FILE
+> ./mysql -uroot -p /home/cmwin/sql/create_tables.sql
+
+mysql> show variables like 'low_case_table_names'
